a120abce78307cb44867a8ccafe9556b88608b16
leanne_front <leanne_front@cxgi.org>
2021-01-19 17:39:53 +0800

    project init

---
+++ Vue/demo/src/components/DisplayNumbers.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>{{ storeState }}</h2>
+  </div>
+</template>
+
+<script>
+import {store} from '../store.js'
+
+export default {
+    data() {
+        return {
+            storeState: store.state.numbers
+            }
+    },
+    methods: {
+       
+    },
+    created(){ 
+        console.log(store)
+    }
+}
+</script>
